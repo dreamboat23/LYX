031e5afe22c328d05ae717c8661db66ec98df515
liuyongxing <yongxing.liu@hpe.com>
2020-09-10 14:09:16 +0800

    test ok

--- Spring5/spring5_demo1/src/testdemo/UserTest.java
+++ Spring5/spring5_demo1/src/testdemo/UserTest.java
@@ -1,4 +1,4 @@
-package com.yongx.spring5.test;
+package testdemo;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -10,10 +10,8 @@ public class UserTest {
 
 	@Test
 	public void testAdd() {
-		//1 加载spring配置文件
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("bean1.xml");
-		//2 获取配置创建的对象
 		User user = context.getBean("user", User.class);
 		System.out.println(user);
 		user.add();
