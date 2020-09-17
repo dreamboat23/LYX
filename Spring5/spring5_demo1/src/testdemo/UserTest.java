85cc38f514464e3eff91c53852b44b8ce5a9a9fc
liuyongxing <yongxing.liu@hpe.com>
2020-09-17 09:46:40 +0800

    update test class

--- Spring5/spring5_demo1/src/testdemo/UserTest.java
+++ Spring5/spring5_demo1/src/testdemo/UserTest.java
@@ -10,12 +10,14 @@ public class UserTest {
 
 	@Test
 	public void testAdd() {
+		//1 加载spring配置文件
 		//ApplicationContext：代表IOC容器
 		//ClassPathXmlApplicationContext：当前应用的xml配置在classpath内路径下
 		//创建ioc容器
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("bean1.xml");
 
+		//2 获取配置创建的对象
 		//容器帮我们创建好对象了，根据xml中的id拿值
 		User user = context.getBean("user", User.class);
 		System.out.println(user);
