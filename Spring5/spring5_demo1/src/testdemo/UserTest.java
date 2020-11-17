2cdf1de566de0951c67cc69308397489647cf302
liuyongxing <yongxing.liu@hpe.com>
2020-11-17 15:18:19 +0800

    update conf IOC（BeanFactory接口）

--- Spring5/spring5_demo1/src/testdemo/UserTest.java
+++ Spring5/spring5_demo1/src/testdemo/UserTest.java
@@ -3,6 +3,7 @@ package testdemo;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.yongx.spring5.User;
 
@@ -15,7 +16,10 @@ public class UserTest {
 		//ClassPathXmlApplicationContext：当前应用的xml配置在classpath内路径下
 		//创建ioc容器
 		ApplicationContext context = 
-				new ClassPathXmlApplicationContext("bean1.xml");
+				new FileSystemXmlApplicationContext("src/bean1.xml"); 
+
+		//ApplicationContext context = 
+			//	new ClassPathXmlApplicationContext("bean1.xml");
 
 		//2 获取配置创建的对象
 		//容器帮我们创建好对象了，根据xml中的id拿值
